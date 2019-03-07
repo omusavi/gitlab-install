25c7d76451b3d0d2ae848db35fafc981542e3548
Omeed Musavi <omusavi@microsoft.com>
2019-03-07 15:52:53 -0800

    Change IP to FQDN

--- gitlab-config.sh
+++ gitlab-config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-ip=$1
+fqdn=$1
+
 
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y curl openssh-server ca-certificates
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
-sudo EXTERNAL_URL="http://${ip}" apt-get install gitlab-ee
+sudo EXTERNAL_URL="http://${fqdn}" apt-get install -y gitlab-ee
