9d016d6ae1512465abfb719c639f00a14c943984
Omeed Musavi <omusavi@microsoft.com>
2019-03-07 13:08:44 -0800

    Add environment variable to prevent openssh-server from requiring user input

--- gitlab-config.sh
+++ gitlab-config.sh
@@ -2,6 +2,6 @@
 ip=$1
 
 sudo apt-get update
-sudo apt-get install -y curl openssh-server ca-certificates
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y curl openssh-server ca-certificates
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
 sudo EXTERNAL_URL="http://${ip}" apt-get install gitlab-ee
