166e1f46c3fa8527d2f776f86b4c893a6d3d8317
Omeed Musavi <omusavi@microsoft.com>
2019-03-07 12:40:44 -0800

    Add IP param and install

--- gitlab-config.sh
+++ gitlab-config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+ip=$1
 
 sudo apt-get update
 sudo apt-get install -y curl openssh-server ca-certificates
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
+sudo EXTERNAL_URL="http://${ip}" apt-get install gitlab-ee
